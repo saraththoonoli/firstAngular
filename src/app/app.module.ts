c77e959fb446ae550cb3292c70cc64beb8e1fa93
saraththoonoli <thoonolisarath@gmail.com>
2023-07-18 00:59:46 +0530

    workone

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { LeftComponent } from './left/left.component';
+import { RightComponent } from './right/right.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LeftComponent,
+    RightComponent
   ],
   imports: [
     BrowserModule,
